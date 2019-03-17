5af9fb19e668521717df8ad2b31e88c861ecfcf3
Jakub Sapalski <coosheck@gmail.com>
2019-03-17 22:02:20 +0100

    Format the code

--- src/message/message.rs
+++ src/message/message.rs
@@ -4,5 +4,5 @@ use serde::Serialize;
 #[derive(Serialize)]
 #[serde(rename_all = "camelCase", tag = "type", content = "data")]
 pub enum Message {
-    Register(RegisterData)
+    Register(RegisterData),
 }
